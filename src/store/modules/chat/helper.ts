dfea7b664c9c26edb3fcd02ac459ad88b37cc9f3
hpstream <915414300@qq.com>
2023-04-09 09:54:29 +0800

    Revert "feat: 🎸 支持vscode 离线历史记录功能"

--- src/store/modules/chat/helper.ts
+++ src/store/modules/chat/helper.ts
@@ -1,30 +1,17 @@
 import { ss } from '@/utils/storage'
 
 const LOCAL_NAME = 'chatStorage'
-let vscode:any = null;
 
-if (typeof acquireVsCodeApi !== 'undefined') {
-   vscode = acquireVsCodeApi();
- 
-}
 export function defaultState(): Chat.ChatState {
   const uuid = 80
   return { active: uuid, history: [{ uuid, title: '新建问题', isEdit: false }], chat: [{ uuid, data: [] }] }
 }
 
 export function getLocalState(): Chat.ChatState {
-  const localState = window.chatStorage ?? ss.get(LOCAL_NAME)
+  const localState = ss.get(LOCAL_NAME)
   return localState ?? defaultState()
 }
 
 export function setLocalState(state: Chat.ChatState) {
-
-  if (vscode){
-    vscode.postMessage({
-      type: "chatStorage",
-      value: JSON.stringify(state),
-    });
-  }
-  console.log(state)
   ss.set(LOCAL_NAME, state)
 }
