778d8bd1311e20e891967dd36e85ae3a5b68b6e6
duoluodexiaoxiaoyuan <1203125214@qq.com>
2023-06-04 16:56:34 +0800

    fix: 修复会话记录不显示的问题

--- src/views/chat/layout/sider/index.vue
+++ src/views/chat/layout/sider/index.vue
@@ -1,7 +1,7 @@
 <script setup lang='ts'>
 import type { CSSProperties } from 'vue'
 import { computed, ref, watch } from 'vue'
-import { NLayoutSider, NPopconfirm , NButton } from 'naive-ui'
+import { NLayoutSider, NPopconfirm , NButton, NInput } from 'naive-ui'
 // import Tips from '../../tips.vue'
 
 import List from './List.vue'
@@ -79,7 +79,7 @@ watch(
               新建问题
             </NButton>
           </div>
-          
+
           <NPopconfirm placement="bottom" @positive-click="()=> chatStore.deleteBatchHistory()">
             <template #trigger>
               <NButton id="question-btn2" dashed block @click.stop>
