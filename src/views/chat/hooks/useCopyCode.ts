b60341e69e6d1f0230de3f1639be6e0bd3228211
hpstream <huangpeng_2022@qq.com>
2023-06-13 22:37:25 +0800

    feat: 🎸 提交代码

--- src/views/chat/hooks/useCopyCode.ts
+++ src/views/chat/hooks/useCopyCode.ts
@@ -1,18 +1,30 @@
-import { onMounted, onUpdated } from 'vue'
+import { onMounted, onUpdated, computed } from 'vue'
 import { copyText } from '@/utils/format'
+import {
+  createDiscreteApi
+} from 'naive-ui'
+// const configProviderPropsRef = computed<ConfigProviderProps>(() => ({
+//   theme: themeRef.value === 'light' ? lightTheme : darkTheme
+// }))
 
+const { message } = createDiscreteApi(
+  ['message', 'dialog', 'notification', 'loadingBar'],
+)
 export function useCopyCode() {
   function copyCodeBlock() {
     const codeBlockWrapper = document.querySelectorAll('.code-block-wrapper')
     codeBlockWrapper.forEach((wrapper) => {
       const copyBtn = wrapper.querySelector('.code-block-header__copy')
       const codeBlock = wrapper.querySelector('.code-block-body')
+      // console.log(copyBtn, codeBlock)
       if (copyBtn && codeBlock) {
         copyBtn.addEventListener('click', () => {
+
           if (navigator.clipboard?.writeText)
             navigator.clipboard.writeText(codeBlock.textContent ?? '')
           else
             copyText({ text: codeBlock.textContent ?? '', origin: true })
+          message.success('已复制到剪切板')
         })
       }
     })
