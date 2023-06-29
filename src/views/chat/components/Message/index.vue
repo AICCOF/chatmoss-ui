32496c95905f8b34a93be7448c8c553b9b0b90c2
hpstream <huangpeng_2022@qq.com>
2023-06-29 14:19:59 +0800

    feat: 🎸 优化问题

--- src/views/chat/components/Message/index.vue
+++ src/views/chat/components/Message/index.vue
@@ -92,7 +92,7 @@ function handleSelect(key: string, askMsg: string) {
     </div>
     <div class="overflow-hidden text-sm " :class="[inversion ? 'items-end' : 'items-start']">
       <p class="text-xs" :class="[inversion ? 'text-right' : 'text-left']">
-        {{ dateTime }} <span v-if="chatStore.active">({{ chatStore.active }}) </span>
+        {{ dateTime }} <span v-if="chatStore.active">会话ID:({{ chatStore.active }}) </span>
       </p>
       <p v-if="!inversion && viewMsg" class="text-xs mt-1" :class="[inversion ? 'text-right' : 'text-left']">
         <span>{{ viewMsg }} </span>
